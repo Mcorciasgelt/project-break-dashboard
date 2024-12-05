d4e6387538fa492cdfb932b2ee1cbe53839daa0e
Michael Corcias <michael.corcias@gelt.com>
2024-12-06 00:27:16 +0100

    reloj versión 1

---
+++ js/clock.js
@@ -0,0 +1,80 @@
+const divElementoActivo = document.getElementById("divReloj")
+const divMensaje = document.getElementById("divMensaje")
+
+const mostrarFecha = setInterval(sacarFecha, 1000)
+
+function sacarFecha() {
+    const fecha = new Date()
+    const dia = String(fecha.getDate()).padStart(2, '0')
+    const mes = fecha.getMonth()+1
+    const año = fecha.getFullYear()
+    const hora = String(fecha.getHours()).padStart(2, '0')
+    const minutos = String(fecha.getMinutes()).padStart(2, '0')
+    const segundos = String(fecha.getSeconds()).padStart(2, '0')
+
+
+    const fechaActual = `${dia}/${mes}/${año}`
+    const horaActual = `${hora}:${minutos}:${segundos}`
+
+    divElementoActivo.innerHTML = 
+        
+        `
+        <h1>Hoy es el ${fechaActual}</h1>
+        <h2>y la hora es ${horaActual}</h2>
+        `
+    if (hora >= 0 && hora < 7) {
+
+        divMensaje.innerHTML =
+        `
+        <h3>¡Que c*** haces aquí! Anda a descansar!</h3>
+        `
+    }
+    
+    else if (hora >= 7 && hora < 12 ) {
+
+        divMensaje.innerHTML =
+        `
+        <h3>Al que madruga, d.os lo ayuda... eso dicen... pero tu al código!</h3>
+        `
+    }
+    
+    else if (hora >= 12 && hora < 14) {
+
+        divMensaje.innerHTML =
+        `
+        <h3>Si si si, es hora de comer algo bueno!</h3>
+        `
+    }
+
+    else if (hora >= 14 && hora < 16) {
+
+        divMensaje.innerHTML =
+        `
+        <h3>¿Qué tal estuvo la comida? Venga, a por café y seguimos!</h3>
+        `
+    }
+
+    else if (hora >= 16 && hora < 18) {
+
+        divMensaje.innerHTML =
+        `
+        <h3>Vamos, dale un poco más que seguro que lo consigues!!</h3>
+        `
+    }
+
+    else if (hora >= 18 && hora < 22) {
+
+        divMensaje.innerHTML =
+        `
+        <h3>No paras, son buenas horas para ir a comer algo y ya seguir mañana!</h3>
+        `
+    }
+
+    else if (hora >= 22 && hora <= 23) {
+
+        divMensaje.innerHTML =
+        `
+        <h3>Una peli y a la cama!</h3>
+        `
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+const divElementoActivo = document.getElementById("divReloj")
+
+const mostrarFecha = setInterval(sacarFecha, 1000)
+
+function sacarFecha() {
+    const fecha = new Date()
+    const dia = fecha.getDay()
+    const mes = fecha.getMonth()
+    const año = fecha.getFullYear()
+
+    const fechaActual = `${dia}/${mes}/${año}`
+
+    console.log(fechaActual)
+
+    divElementoActivo.innerText = fechaActual
+}
